ce3e566a6bd78eb8b791ae2ed21e61b194434400
Marc Mills <mdmills1990@gmail.com>
2018-09-14 14:40:13 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ def get_page
   doc.css(".post").each do |post|
     course = Course.new
     course.title = post.css("h2").text
-    course.date = post.css(".date").text
+    course.schedule = post.css(".date").text
     course.description = post.css("p").text
   end
   binding.pry
