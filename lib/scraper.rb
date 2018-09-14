3ad643e63e2126af2308e8906eab0d71cb1e5960
Marc Mills <mdmills1990@gmail.com>
2018-09-14 14:12:08 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,6 +15,13 @@ class Scraper
       end
     end
   end
+
+def get_page
+end
+
+def get_courses
+end
+  
   
 end
 
