a8ef50649052c000ec9c8204324fc9e79bc27f68
Marc Mills <mdmills1990@gmail.com>
2018-09-14 14:12:17 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -21,9 +21,9 @@ end
 
 def get_courses
 end
-  
-  
+
+def make_courses
 end
 
 
-
+end
